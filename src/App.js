cce894af31905d4284d7ac5b95930a7b08f8e66b
Yavuz Selim Şerifoğlu <70685127+serifselim@users.noreply.github.com>
2022-08-08 09:15:18 +0300

    route was transfared to app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { Hello, Login, Main, Register } from './pages';
+// Redux
+import { useSelector } from 'react-redux';
 // Router
-import Router from './router';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
 
+  const { user } = useSelector(state => state.auth);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+      if (!user && location.pathname !== '/login' && location.pathname !== '/register') {
+          navigate('/')
+      } else if (user && (location.pathname === '/login' || '/register')) {
+          navigate('/main')
+      }
+  }, [])
+
   return (
-    <Router />
+    <Routes>
+        <Route path="/" element={<Hello />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/main" element={<Main />} />
+    </Routes>
   );
 };
 
