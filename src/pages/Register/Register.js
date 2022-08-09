e55d35a608d0a71cf2d4425ca1af6fd48ed3f28a
mervegolpinar <mervegolpinar7@gmail.com>
2022-08-09 16:34:53 +0300

    login/register page tasarım revize

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,7 +1,7 @@
-import { Button, Center, Container, Image, Input, Text, Link } from "@chakra-ui/react"
+import { Button, Center, Container, Image, Input, Text, Link, Flex } from "@chakra-ui/react"
 import { React, useState } from "react"
 import { register } from "../../firebase"
-import logo from '../../assets/logo.png';
+import sign from '../../assets/sign.png';
 
 export default function Register() {
 
@@ -17,20 +17,25 @@ export default function Register() {
   }
   return (
     <form onSubmit={handleSubmit}>
-      <Center h='800px'>
-        <Container maxW='300px' color='black'>
-          <Input border='2px' type='text' value={userName} placeholder='Kullanıcı Adı' mt={4} mb={4} onChange={e => setUserName(e.target.value)} />
-          <Input border='2px' type='text' value={email} placeholder='E-Mail' onChange={e => setEmail(e.target.value)} />
-          <Input border='2px' type='password' value={password} mt={4} mb={4} placeholder='Şifre' onChange={e => setPassword(e.target.value)} />
-          <Input border='2px' type='password' value={againPassword} mb={4} placeholder='Şifre Tekrarı' onChange={e => setAgainPassword(e.target.value)} />
-          <Center>
+      <Center h='400px'>
+        <Container maxW='400px' color='black'>
+          <Flex direction="column" alignItems="center">
+            <Image boxSize='120px' mb={4} src={sign} />
+            <Text lineHeight='110%' mb={5} as='sub' fontSize='sm' color='black'>
+              "Sandık'ta oylamaya bende varım!" diyorsan hemen kaydol </Text>
+            <Input border='2px' type='text' value={userName} placeholder='Kullanıcı Adı' mt={4} mb={4} onChange={e => setUserName(e.target.value)} />
+            <Input border='2px' type='text' value={email} placeholder='E-Mail' onChange={e => setEmail(e.target.value)} />
+            <Input border='2px' type='password' value={password} mt={4} mb={4} placeholder='Şifre' onChange={e => setPassword(e.target.value)} />
+            <Input border='2px' type='password' value={againPassword} mb={4} placeholder='Şifre Tekrarı' onChange={e => setAgainPassword(e.target.value)} />
             <Button type="submit" disabled={!email || !password || !userName || !againPassword} colorScheme='red' size='md'>
               Kayıt Ol
             </Button>
+          </Flex>
+          <Center>
+            <Text mt={5} as='sub' fontSize='sm' color='black' >
+              Hesabın zaten var mı ? O zaman <Link color="red" fontWeight='bold' href='/login'>Giriş Yap</Link>
+            </Text>
           </Center>
-          <Text mt={5} as='sub' fontSize='sm' color='black' >
-            Hesabın zaten var mı ? O zaman <Link color="red" fontWeight='bold' href='/login'>Giriş Yap</Link>
-          </Text>
         </Container>
       </Center>
     </form>
